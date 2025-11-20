0294ee2202b9ed29965cdafa123056c52b7dc65f
Максим Сыров <m.syrov@it-bastion.com>
2025-11-20 10:23:17 +0300

    refactored background.js

--- src/background/background.js
+++ src/background/background.js
@@ -1,39 +1,42 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    sendTabsMessages(request, sendResponse)
-})
-chrome.tabs.onUpdated.addListener(tabId => {
-    // Проверяем, что URL вкладки допустим
-    chrome.storage.sync.get('lastRequest').then(items => {
-        // chrome.tabs.sendMessage(tab.id, items.lastRequest)
-        chrome.scripting
-            .executeScript({
-                target: { tabId },
-                files: ['content/content.js'],
-            })
-            .then(() => {
-                chrome.tabs.sendMessage(tabId, items.lastRequest)
-                // sendResponse?.(items.lastRequest.value)
-            })
+loadContent = async tabId => {
+    await chrome.scripting.executeScript({
+        target: { tabId },
+        files: ['content/content.js'],
     })
-})
+}
+
+sendTabMessage = async (tabId, request, count = 0) => {
+    try {
+        if (count > 10) return
+        await chrome.tabs.sendMessage(tabId, request)
+    } catch (err) {
+        // console.error(err, count)
+        await loadContent(tabId)
+        await sendTabMessage(tabId, request, count++)
+    }
+}
 
 sendTabsMessages = (request, sendResponse = null) => {
     chrome.tabs.query({ currentWindow: true }, tabs => {
-        tabs.forEach(tab => {
-            if (!tab) return console.error('No active tab')
-            chrome.scripting
-                .executeScript({
-                    target: { tabId: tab.id },
-                    files: ['content/content.js'],
-                })
-                .then(() => {
-                    chrome.tabs.sendMessage(tab.id, request)
-                    sendResponse?.({ status: 'success', value: request.value })
-                })
+        tabs.forEach(async tab => {
+            // if (!tab) return console.error('No active tab')
+            await sendTabMessage(tab.id, request)
+            sendResponse?.({ status: 'success', value: request.value })
         })
     })
     return true
 }
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    sendTabsMessages(request, sendResponse)
+})
+
+chrome.tabs.onUpdated.addListener(tabId => {
+    chrome.storage.sync.get('lastRequest').then(items => {
+        sendTabMessage(tabId, items.lastRequest)
+    })
+})
 
 chrome.storage.onChanged.addListener((changes, area) => {
     if (area === 'sync' && changes.lastRequest?.newValue) {
